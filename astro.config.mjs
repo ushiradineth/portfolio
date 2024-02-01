9685b31c9bf025ca8a53f83cf71ea244e3aa4fe0
Ushira Dineth <ushiradineth@gmail.com>
2024-02-02 00:40:45 +0530

    fix(image): fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap(), robotsTxt()]
+	integrations: [react(), tailwind(), sitemap(), robotsTxt()],
+	output: "static",
+	adapter: vercelStatic(),
 });
