ecd0745d61f7483c8f4b75c88d4453652c160b98
Ushira Dineth <ushiradineth@gmail.com>
2024-02-02 00:31:39 +0530

    feat: update all sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,21 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
-        roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+		extend: {
+			colors: {
+				background: "var(--background)",
+				text: "var(--text)",
+				card: "var(--card)",
+				border: "var(--border)",
+				muted: "var(--muted)",
+			},
+			fontFamily: {
+				montserrat: ['"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
+				roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 	plugins: [],
 };
