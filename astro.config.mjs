2f1857de55f0cdf2da0220959d401e2e047989df
Ushira Dineth <ushiradineth@gmail.com>
2024-02-05 10:10:22 +0530

    chore: removed preload and compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import vercelStatic from "@astrojs/vercel/static";
-import preload from "astro-preload";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), preload(), compress()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   output: "static",
   site: 'https://ushira.com',
   adapter: vercelStatic()
