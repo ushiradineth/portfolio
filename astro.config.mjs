eb36198711cebfafe9acbae68d561094b46a8395
Ushira Dineth <ushiradineth@gmail.com>
2024-02-05 09:50:03 +0530

    chore: added image optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import vercelStatic from "@astrojs/vercel/static";
+import preload from "astro-preload";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind(), sitemap(), robotsTxt()],
-	output: "static",
-	site: 'https://ushira.com',
-	adapter: vercelStatic(),
+  integrations: [tailwind(), sitemap(), robotsTxt(), preload(), compress()],
+  output: "static",
+  site: 'https://ushira.com',
+  adapter: vercelStatic()
 });
