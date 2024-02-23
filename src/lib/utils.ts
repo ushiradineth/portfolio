3894ad1c09666d4ab48ecd95e5ca2da55a7d75f5
Ushira Dineth <ushiradineth@gmail.com>
2024-02-23 20:30:30 +0530

    fix: made blog item more responsive

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,34 +7,42 @@ export function border(index: number, length: number) {
 }
 
 export function getDurationFromDate(date: Date): string {
-  // Current date
-  const currentDate = new Date();
-  
-  // Calculate the difference in milliseconds
-  const difference_ms = currentDate.getTime() - date.getTime();
+	// Current date
+	const currentDate = new Date();
 
-  // Convert the difference to years and months
-  const difference_years = Math.floor(difference_ms / (1000 * 60 * 60 * 24 * 365));
-  const difference_months = Math.floor((difference_ms % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+	// Calculate the difference in milliseconds
+	const difference_ms = currentDate.getTime() - date.getTime();
 
-  // Format the result
-  let result = '';
-  if (difference_years !== 0) {
-    result += difference_years + " year";
-    if (difference_years !== 1) {
-      result += "s";
-    }
-  }
-  if (difference_months !== 0) {
-    if (result !== '') {
-      result += " and ";
-    }
-    result += difference_months + " month";
-    if (difference_months !== 1) {
-      result += "s";
-    }
-  }
+	// Convert the difference to years and months
+	const difference_years = Math.floor(difference_ms / (1000 * 60 * 60 * 24 * 365));
+	const difference_months = Math.floor((difference_ms % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
 
-  return result;
+	// Format the result
+	let result = "";
+	if (difference_years !== 0) {
+		result += difference_years + " year";
+		if (difference_years !== 1) {
+			result += "s";
+		}
+	}
+	if (difference_months !== 0) {
+		if (result !== "") {
+			result += " and ";
+		}
+		result += difference_months + " month";
+		if (difference_months !== 1) {
+			result += "s";
+		}
+	}
+
+	return result;
 }
 
+export function formatDate(date: Date): string {
+	const options: Intl.DateTimeFormatOptions = {
+		day: "2-digit",
+		month: "short",
+		year: "numeric",
+	};
+	return new Intl.DateTimeFormat("en-US", options).format(date);
+}
