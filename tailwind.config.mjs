c59bc0e3164a064fea84ad5382b444013ede4801
Ushira Dineth <ushiradineth@gmail.com>
2024-09-20 22:51:15 +0400

    feat: add border color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,23 +2,24 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "class",
-	theme: {
-		extend: {
-			colors: {
-				text: "var(--text)",
-				background: "var(--background)",
-				primary: "var(--primary)",
-				secondary: "var(--secondary)",
-				accent: "var(--accent)",
-				muted: "var(--muted)",
-			},
-			fontFamily: {
-				montserrat: ['"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
-				roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        muted: "var(--muted)",
+        border: "var(--border)",
+      },
+      fontFamily: {
+        montserrat: ['"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
+        roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 };
