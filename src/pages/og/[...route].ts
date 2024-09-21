af4503dc710341b4b0b02e530e054892e327581d
Ushira Dineth <ushiradineth@gmail.com>
2024-09-22 00:44:20 +0400

    feat: update opengraph images

--- src/pages/og/[...route].ts
+++ src/pages/og/[...route].ts
@@ -1,37 +1,41 @@
 import { OGImageRoute } from "astro-og-canvas";
 
 export const { getStaticPaths, GET } = OGImageRoute({
-	param: "route",
+  param: "route",
 
-	pages: {
-		index: {
-			title: "Hey, I'm Ushira!",
-			description: "ushira.com",
-		},
-		projects: {
-			title: "Projects",
-			description: "ushira.com",
-		},
-		work: {
-			title: "Work Experience",
-			description: "ushira.com",
-		},
-		blog: {
-			title: "Blog Posts",
-			description: "ushira.com",
-		},
-	},
+  pages: {
+    index: {
+      title: "portfolio",
+    },
+    projects: {
+      title: "projects",
+    },
+    work: {
+      title: "work",
+    },
+    blog: {
+      title: "blog",
+    },
+  },
 
-	getImageOptions: (_, page) => ({
-		title: page.title,
-		description: page.description,
-		logo: {
-			path: "./src/images/self.png",
-			size: [300, 300],
-		},
-		bgImage: {
-			path: "./src/images/og-bg.png",
-			fit: "cover",
-		},
-	}),
+  getImageOptions: (_, page) => ({
+    title: page.title,
+    description: page.description,
+    bgImage: {
+      path: "./src/images/og-bg.png",
+    },
+    fonts: ["https://cdn.jsdelivr.net/fontsource/fonts/roboto-mono:vf@latest/latin-wght-normal.woff2"],
+    font: {
+      title: {
+        color: [0, 0, 0],
+        families: ["Roboto Mono"],
+        size: 80,
+      },
+      description: {
+        color: [0, 0, 0],
+        families: ["Roboto Mono"],
+        size: 30,
+      },
+    },
+  }),
 });
