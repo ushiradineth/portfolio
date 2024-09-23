1cd1d4c7d0cf23d505c9b202cd61951fbf235327
Ushira Dineth <ushiradineth@gmail.com>
2024-09-23 17:16:01 +0400

    feat: add structured data

---
+++ src/lib/data/structuredData.ts
@@ -0,0 +1,71 @@
+import { type Article, type Person, type WebSite, type WithContext } from "schema-dts";
+import avatar from "../../images/self.png";
+import type { CollectionEntry } from "astro:content";
+import { SocialLinks } from "./socials";
+
+export const blogWebsite: WithContext<WebSite> = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  url: `${import.meta.env.SITE}/blog/`,
+  name: "Dzmitry Kozhukh blog",
+  description: "Frontend insights",
+  inLanguage: "en_US",
+};
+
+export const mainWebsite: WithContext<WebSite> = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  url: import.meta.env.SITE,
+  name: "Ushira Dineth Portfolio",
+  description: "Ushira Dineth's contact page, portfolio and blog",
+  inLanguage: "en_US",
+};
+
+export const personSchema: WithContext<Person> = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Ushira Dineth",
+  url: "https://ushira.com",
+  image: `${import.meta.env.SITE}${avatar.src}`,
+  sameAs: [
+    SocialLinks.get("Twitter")!,
+    SocialLinks.get("LinkedIn")!,
+    SocialLinks.get("Github")!,
+    SocialLinks.get("CV")!,
+    SocialLinks.get("YouTube")!,
+  ],
+  jobTitle: "Software Engineer",
+  worksFor: {
+    "@type": "Organization",
+    name: "Surge Global",
+    url: "https://surge.global",
+  },
+};
+
+export function getArticleSchema(post: CollectionEntry<"blog">) {
+  const articleStructuredData: WithContext<Article> = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    headline: post.data.title,
+    url: `${import.meta.env.SITE}/blog/${post.slug}/`,
+    image: {
+      "@type": "ImageObject",
+      url: `${import.meta.env.SITE}/og/blog/${post.slug}/`,
+    },
+    description: post.data.description,
+    datePublished: post.data.date.toString(),
+    publisher: {
+      "@type": "Person",
+      name: "Ushira Dineth",
+      url: import.meta.env.SITE,
+      image: import.meta.env.SITE + avatar.src,
+    },
+    author: {
+      "@type": "Person",
+      name: "Ushira Dineth",
+      url: import.meta.env.SITE,
+      image: import.meta.env.SITE + avatar.src,
+    },
+  };
+  return articleStructuredData;
+}
