289cb9217370f1bf5015807e0b475a12a69c5a56
Ushira Dineth <ushiradineth@gmail.com>
2025-05-18 01:16:06 +0400

    style: fmt

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,25 +2,25 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        text: "rgb(var(--text))",
-        background: "rgb(var(--background))",
-        primary: "rgb(var(--primary))",
-        accent: {
-          DEFAULT: "rgb(var(--accent))",
-          contrast: "rgb(var(--accent-contrast))",
-        },
-        muted: "rgb(var(--muted))",
-        border: "rgb(var(--border))",
-      },
-      fontFamily: {
-        roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				text: "rgb(var(--text))",
+				background: "rgb(var(--background))",
+				primary: "rgb(var(--primary))",
+				accent: {
+					DEFAULT: "rgb(var(--accent))",
+					contrast: "rgb(var(--accent-contrast))",
+				},
+				muted: "rgb(var(--muted))",
+				border: "rgb(var(--border))",
+			},
+			fontFamily: {
+				roboto: ['"Roboto Mono Variable"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
